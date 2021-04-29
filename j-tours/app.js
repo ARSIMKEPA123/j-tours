d53ae383761a32efa4dfe91fb1be5d64ac8e298e
ARSIMKEPA123 <arsimkepa@gmail.com>
2021-04-29 19:18:09 +0200

    POST & GET

--- j-tours/app.js
+++ j-tours/app.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 const app = express()
 
 
+app.use(express.json())
+
+
 app.get ('/'  , (req,res) => {
 
     res.json({message: "hello from the server side",
@@ -17,15 +20,44 @@ app.get ('/'  , (req,res) => {
 } )
 
 
-const tours = JSON.parse(fs.readFileSync('${__dirname}/dev-data/data/tours-simple.js'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+app.get('/api/v1/tours', (req,res)=>{
+
+    res.json({
+
+        status: "success",
+        data: {tours}
+
+    })
+
+})
 
 
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
 
+    // ka me u shtu nje dokument i ri
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: newId }, req.body)
 
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        })
+
+    })
+
+})
 app.listen(3000 , () => {
 
     console.log('server is listening')
 })
 
-git config --global user.name 'ARSIMKEPA123'
-git config --global user.email 'arsimkepa@gmail.com'
+// git config --global user.name 'ARSIMKEPA123'
+// git config --global user.email 'arsimkepa@gmail.com'
+
